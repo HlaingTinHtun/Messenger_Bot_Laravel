dfb3538d908a6c840aef818683a8f994462a1eb8
hlaingtinhtun <hlaingtinhtun@gmail.com>
2018-12-02 16:48:58 +0630

    adding functions in controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+  public function receive(Request $request)
+  {
+      $data = $request->all();
+      //get the user’s id
+      $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
+      $this->sendTextMessage($id, "Hello");
+  }
+
+  private function sendTextMessage($recipientId, $messageText)
+  {
+    $messageData = [
+        "recipient" => [
+            "id" => $recipientId
+        ],
+        "message" => [
+            "text" => $messageText
+        ]
+    ];
+    $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . env("PAGE_ACCESS_TOKEN"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($messageData));
+    curl_exec($ch);
+
+  }
+
 }
